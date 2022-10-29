2e6b51522bc64ca2f89e7adcc29955c741a1dd84
Sailesh Dev <saileshdev@gmail.com>
2022-10-29 23:35:24 +0530

    Local network setup

--- smartbunk/settings.py
+++ smartbunk/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.29.84',
+]
 
 
 # Application definition
