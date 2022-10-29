cc45f00a25799b9643431e2f7fe0992f0fac11bd
Sailesh Dev <saileshdev@gmail.com>
2022-10-30 01:17:29 +0530

    Add razorpay endpoints

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('fetchAttendance/', views.fetch_attendance),
+    path('createOrder/', views.create_order),
+    path('verifySignature/', views.verify_signature),
 ]
--- api/views.py
+++ api/views.py
@@ -3,6 +3,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import SBUser, Payment, APIUsage
+import razorpay
+import os
 
 # Create your views here.
 
@@ -52,3 +54,59 @@ def fetch_attendance(request):
         sb_user.save()
         message = scrape_data(sb_user)
         return Response({'message': message})
+
+
+@api_view(['POST'])
+def create_order(request):
+    global client
+    data = request.data
+
+    amount = int(float(data['amount']))
+    key_id = str(os.getenv('KEY_ID'))
+    key_secret = str(os.getenv('KEY_SECRET'))
+    client = razorpay.Client(auth=(key_id, key_secret))
+
+    data = {"amount": amount, "currency": "INR"}
+    payment = client.order.create(data=data)
+
+    return Response({'order_id': payment['id'], 'amount': payment['amount'], 'currency': payment['currency']})
+
+
+@api_view(['POST'])
+def verify_signature(request):
+    res = request.data
+
+    user_name = res.get('username')
+    if user_name is None:
+        error = "Please send a valid username"
+        return Response({'error': error}, status=status.HTTP_400_BAD_REQUEST)
+
+    amount = res.get('amount')
+    if amount is None:
+        error = "Please send a valid amount"
+        return Response({'error': error}, status=status.HTTP_400_BAD_REQUEST)
+
+    params_dict = {
+        'razorpay_payment_id': res['razorpay_payment_id'],
+        'razorpay_order_id': res['razorpay_order_id'],
+        'razorpay_signature': res['razorpay_signature']
+    }
+
+    # verifying the signature
+    res = client.utility.verify_payment_signature(params_dict)
+
+    if res is True:
+        # update payment status flag for the user
+        try:
+            sb_user = SBUser.objects.get(user_name=user_name)
+            payment = Payment.objects.get(user=sb_user)
+            payment.payment_status = True
+            payment.save()
+        except Payment.DoesNotExist:
+            payment = Payment(user=sb_user, amount=amount, payment_status=True, payment_id=res['razorpay_payment_id'],
+                              order_id=res['razorpay_order_id'])
+            payment.save()
+
+        return Response({'status': 'Payment Successful'})
+
+    return Response({'status': 'Payment Failed'})
