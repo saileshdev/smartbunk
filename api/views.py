0187b5335398ee58a6f544e5ad0200fc8d9e9ef3
Sailesh Dev <saileshdev@gmail.com>
2022-10-29 23:04:31 +0530

    Created attendance fetch view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+@api_view(['POST'])
+def fetch_attendance(request):
+    user_name = request.data['username']
+    password = request.data['password']
+    return Response({"message": f'{user_name} {password}'})
